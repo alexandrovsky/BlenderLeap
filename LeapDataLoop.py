ac649b01f5c2f23ce16123e7b835d07fc7a3607f
Kolja Lubitz <pinguin999@gmail.com>
2013-05-23 15:40:58 +0200

    Set Bone rotation and keyframe

--- LeapDataLoop.py
+++ LeapDataLoop.py
@@ -9,6 +9,7 @@ class ModalTimerOperator(bpy.types.Operator):
     bl_label = "Modal Timer Operator"
 
     _timer = None
+    _frame_num = 0
     _controller = Leap.Controller()
     _cube = bpy.data.objects['Cube']
 
@@ -18,15 +19,33 @@ class ModalTimerOperator(bpy.types.Operator):
 
         if event.type == 'TIMER':
             frame = self._controller.frame()
-            #print(frame)
+            bpy.context.scene.frame_set(self._frame_num)
+            self._frame_num += 1
             if not frame.hands.empty:
                 hand = frame.hands[0]
                 # Check if the hand has any fingers
                 fingers = hand.fingers
-                self._cube.location.x = fingers[0].tip_position.x / 10.0
-                self._cube.location.z = fingers[0].tip_position.y / 10.0
-                self._cube.location.y = -fingers[0].tip_position.z / 10.0
-
+                #Object Cube move
+#                self._cube.location.x = fingers[0].tip_position.x / 10.0
+#                self._cube.location.z = fingers[0].tip_position.y / 10.0
+#                self._cube.location.y = -fingers[0].tip_position.z / 10.0
+                
+                #Bones
+                #get all Bones
+                #Anzahl
+                #len(bpy.data.objects['Armature'].pose.bones)
+                #Rotate one Pos
+                bpy.data.objects['Armature'].pose.bones[0].rotation_quaternion.x = fingers[0].direction.x
+                bpy.data.objects['Armature'].pose.bones[0].rotation_quaternion.z = fingers[0].direction.y
+                
+                
+                bpy.data.objects['Armature'].pose.bones[1].rotation_quaternion.x = fingers[1].direction.x
+                bpy.data.objects['Armature'].pose.bones[1].rotation_quaternion.z = fingers[1].direction.y
+                #bpy.data.objects['Armature'].pose.bones[0].rotation_quaternion.z = fingers[0].direction.z
+                print (fingers[1].direction.x)
+                
+                #Add key Frame
+                bpy.data.objects['Armature'].pose.bones[0].keyframe_insert("rotation_quaternion")
         return {'PASS_THROUGH'}
 
     def execute(self, context):
@@ -36,6 +55,9 @@ class ModalTimerOperator(bpy.types.Operator):
 
     def cancel(self, context):
         context.window_manager.event_timer_remove(self._timer)
+        bpy.data.objects['Armature'].pose.bones[0].rotation_quaternion.x = 0
+        bpy.data.objects['Armature'].pose.bones[0].rotation_quaternion.y = 0
+        bpy.data.objects['Armature'].pose.bones[0].rotation_quaternion.z = 0
         return {'CANCELLED'}
 
 
